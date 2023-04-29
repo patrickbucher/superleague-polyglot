25c37b85e036d2b29118a682cae987259de535f2
Patrick Bucher <patrick.bucher@mailbox.org>
2023-04-29 22:08:35 +0200

    transform results into team results

--- superleague-rust/src/main.rs
+++ superleague-rust/src/main.rs
@@ -6,11 +6,18 @@ use std::process::ExitCode;
 
 #[derive(Debug, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Match {
+struct Result {
     home_team: String,
     away_team: String,
     home_goals: u8,
     away_goals: u8,
+}
+
+#[derive(Debug)]
+struct TeamResult {
+    team: String,
+    goals_shot: u8,
+    goals_conceded: u8,
 }
 
 fn main() -> ExitCode {
@@ -22,8 +29,24 @@ fn main() -> ExitCode {
 
     let json_file = &args[1];
     let data = fs::read_to_string(json_file).expect("Should have read file");
-    let matches: Vec<Match> = serde_json::from_str(&data).expect("Should have parsed JSON");
-    dbg!(matches);
+    let results: Vec<Result> = serde_json::from_str(&data).expect("Should have parsed JSON");
+    let team_results: Vec<(TeamResult, TeamResult)> = results.iter().map(|r| {
+        (
+            TeamResult {
+                team: r.home_team.clone(),
+                goals_shot: r.home_goals,
+                goals_conceded: r.away_goals,
+            },
+            TeamResult {
+                team: r.away_team.clone(),
+                goals_shot: r.away_goals,
+                goals_conceded: r.home_goals,
+            },
+        )
+    }).collect();
+    let flat_results: Vec<&TeamResult> = team_results.iter().flat_map(|rs| [&rs.0, &rs.1]).collect();
+
+    dbg!(flat_results);
 
     ExitCode::SUCCESS
 }
